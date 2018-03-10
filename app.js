dce9f7bec0adcdc7935e2833395a2e1ffde576b1
Gregory Lesky <sleepydistrict01@gmail.com>
2018-03-10 11:29:15 -0700

    user routes

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+const cors = require('cors'); //request to api from dif domain name
 const mongoose = require('mongoose');
 
 const app = express();
 
+const users = require('./routes/users');
+
 const port = 3000;
+
+//CORS Middleware
+app.use(cors());
+
+//Body Parser Middleware
+app.use(bodyParser.json());
+
+app.use('/users', users);
 
 app.get('/', (req, res) => {
   res.send('Invald Endpoint');
