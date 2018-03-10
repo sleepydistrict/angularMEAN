6cd1dc19338caf0e94d0b83c12a2f10dfd3a708e
Gregory Lesky <sleepydistrict01@gmail.com>
2018-03-10 12:31:13 -0700

    add User and debug

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user');
 
 //Register
 router.post('/register', (req, res, next) => {
-  res.send('REGISTER');
+  let newUser = new User({
+    name: req.body.name,
+    email: req.body.email,
+    username: req.body.username,
+    password: req.body.password
+  });
+
+  User.addUser(newUser, (err, user) => {
+    if(err){
+      res.json({success: false, msg: 'Im sorry, I was unable to register a new user'});
+    } else {
+      res.json({success: true, msg: 'Welcome New User!'});
+    }
+  })
 });
 
 //Auth
